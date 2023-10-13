94f85e2fb92977b7cb1d9ac8ad3ab5937e4421b1
murateminoglu <murateminoglufb@gmail.com>
2023-10-13 11:44:40 +0200

    port wechseln

--- src/main/java/de/murat/springboot/repository/UserRepository.java
+++ src/main/java/de/murat/springboot/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package de.murat.springboot.repository;
 
 import de.murat.springboot.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, Integer> {
 
